e18795a5700c1d6861e67a7cddfc926dff9e2cef
mummy-alive <mummyee@ewhain.net>
2024-06-28 20:49:27 +0900

    ✨ Tab 기능 추가 - Add Contacts List - Data is hardcoded w data class
    ToDo: Link json or phone data

--- app/src/main/java/com/example/week1/ContactsScreen.kt
+++ app/src/main/java/com/example/week1/ContactsScreen.kt
@@ -13,25 +13,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-/*ToDo: Link json or phone data*/
-data class PERSON(
-    var name: String,
-    var tel: String
-)
-
-val PeopleList: List<PERSON> = List(size = 1000) {
-    PERSON(name = "Person $it", tel = "010-0000-00$it")
-}
-
 @Composable
 fun ContactsScreen(
     modifier: Modifier = Modifier,
-    /* ToDo: Link json or phone data */
-    peopleList: List<PERSON> = PeopleList
-){
+    peopleList: List<PERSON>
+) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)){
 
-        /* ToDo: Link json or phone data */
         for (person in peopleList)
             item {
                 ContactInfo(person)
@@ -39,9 +27,8 @@ fun ContactsScreen(
     }
 }
 
-/*ToDo: Link json or phone data */
 @Composable
-fun ContactInfo(person: PERSON, modifier: Modifier = Modifier) {
+fun ContactInfo(person: PERSON) {
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical =4.dp, horizontal = 8.dp)
