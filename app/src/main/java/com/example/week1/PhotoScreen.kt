69cf7cfd0f09b953febd7fff4389ed36ac71ec59
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-06-28 18:35:14 +0900

    ✨ 사진 기능 추가
    사진 탭의 사진 5개 추가
    사진 파일 5개 추가

--- app/src/main/java/com/example/week1/PhotoScreen.kt
+++ app/src/main/java/com/example/week1/PhotoScreen.kt
@@ -1,15 +1,65 @@
 package com.example.week1
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
+data class Photo(
+    @DrawableRes val imageResourceId: Int,
+    @StringRes val name: Int,
+)
+
+val photos = listOf(
+    Photo(R.drawable.boolguksa, R.string.photo_name_1),
+    Photo(R.drawable.eiffel, R.string.photo_name_2),
+    Photo(R.drawable.greatwall, R.string.photo_name_3),
+    Photo(R.drawable.pyramid, R.string.photo_name_4),
+    Photo(R.drawable.spinx, R.string.photo_name_5)
+)
+
 @Composable
-fun PhotoScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Photo",
-        modifier = modifier.padding(24 .dp)
+fun PhotoScreen(
+    modifier: Modifier = Modifier,
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Adaptive(minSize = 128.dp),
+        verticalArrangement = Arrangement.spacedBy(3.dp),
+        horizontalArrangement = Arrangement.spacedBy(3.dp)
+    ) {
+        items(photos) { photo ->
+            DrawPhoto(id = photo.imageResourceId)
+        }
+    }
+}
+
+@Composable
+fun DrawPhoto(
+    id: Int,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = modifier
+            .padding(1.dp)
+            .aspectRatio(1f / 1f)
+            .background(Color.Yellow)
     )
 }
+
+
