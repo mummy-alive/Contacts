82064a5e2445beb94972450fac125e9a224c91f9
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-06-29 12:13:03 +0900

    ✨️ add Text on Photo

--- app/src/main/java/com/example/week1/PhotoScreen.kt
+++ app/src/main/java/com/example/week1/PhotoScreen.kt
@@ -5,64 +5,95 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.week1.ui.theme.Week1Theme
 
 data class Photo(
     @DrawableRes val imageResourceId: Int,
-    @StringRes val name: Int,
+    @StringRes val nameId: Int,
+    @StringRes val addressId: Int,
 )
 
 val photos = listOf(
-    Photo(R.drawable.boolguksa, R.string.photo_name_1),
-    Photo(R.drawable.eiffel, R.string.photo_name_2),
-    Photo(R.drawable.greatwall, R.string.photo_name_3),
-    Photo(R.drawable.pyramid, R.string.photo_name_4),
-    Photo(R.drawable.spinx, R.string.photo_name_5)
+    Photo(R.drawable.reborn, R.string.photo_name_1, R.string.address_1),
+    Photo(R.drawable.sejong, R.string.photo_name_2, R.string.address_1),
+    Photo(R.drawable.areum, R.string.photo_name_3, R.string.address_1),
+    Photo(R.drawable.plana, R.string.photo_name_4, R.string.address_1),
+    Photo(R.drawable.thebody, R.string.photo_name_5, R.string.address_1),
+    Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
+    Photo(R.drawable.sportscomplex, R.string.photo_name_7, R.string.address_1)
 )
 
 @Composable
 fun PhotoScreen(
     modifier: Modifier = Modifier,
 ) {
-    LazyVerticalGrid(
+    LazyColumn(
         modifier = modifier,
-        columns = GridCells.Adaptive(minSize = 128.dp),
         verticalArrangement = Arrangement.spacedBy(3.dp),
-        horizontalArrangement = Arrangement.spacedBy(3.dp)
     ) {
-        items(photos) { photo ->
-            DrawPhoto(id = photo.imageResourceId)
+        items(photos) { info ->
+            DrawPhoto(photo = info)
         }
     }
 }
 
 @Composable
 fun DrawPhoto(
-    id: Int,
+    photo: Photo,
     modifier: Modifier = Modifier
 ) {
-    Image(
-        painter = painterResource(id = id),
-        contentDescription = null,
-        contentScale = ContentScale.Crop,
-        modifier = modifier
-            .padding(1.dp)
-            .aspectRatio(1f / 1f)
-            .background(Color.Yellow)
-    )
+    Box(
+    ) {
+        Image(
+            painter = painterResource(id = photo.imageResourceId),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+                .padding(1.dp)
+                .aspectRatio(1f / 1f)
+                .background(Color.Yellow)
+        )
+        Text(
+            text = stringResource(id = photo.nameId),
+            fontSize = 40.sp,
+            style = TextStyle(
+                color = Color.White
+            ),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 40.dp)
+        )
+        Text(
+            text = stringResource(id = photo.addressId),
+            fontSize = 20.sp,
+            style = TextStyle(
+                color = Color.White
+            ),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 15.dp)
+        )
+
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
