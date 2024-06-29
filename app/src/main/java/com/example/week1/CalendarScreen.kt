804ee885653e13b903fff90a6489440e0df13328
mummy-alive <mummyee@ewhain.net>
2024-06-29 14:06:11 +0900

    Base format of Calendar - by LazyVerticalGrid

--- app/src/main/java/com/example/week1/CalendarScreen.kt
+++ app/src/main/java/com/example/week1/CalendarScreen.kt
@@ -1,15 +1,46 @@
 package com.example.week1
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CalendarScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Third",
-        modifier = modifier.padding(24 .dp)
-    )
+    CalendarView(modifier)
+}
+
+@Composable
+fun CalendarView(modifier: Modifier){
+    LazyVerticalGrid(
+        modifier=Modifier.fillMaxSize(),
+        columns =GridCells.Adaptive(minSize=68.dp)
+    ) {
+        items(40) { day ->
+            CalendarBox(day, modifier)
+        }
+    }
+}
+
+@Composable
+fun CalendarBox(day: Int, modifier: Modifier){
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.padding(vertical=2.dp, horizontal=2.dp)
+    ) {
+        Column(modifier=Modifier.fillMaxSize()
+            .padding(vertical = 4.dp)){
+            Text(text = day.toString())
+            Spacer(modifier = Modifier.size(30.dp))
+            Text(text = "오운완\uD83D\uDD25")
+        }
+
+    }
+
 }
