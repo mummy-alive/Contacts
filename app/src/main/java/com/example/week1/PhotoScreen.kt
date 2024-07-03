1b061758d41787719c5256ad7625e45865883e3e
wlsdnqqq17 <wlsdnqqq17@gmail.com>
2024-07-03 11:42:29 +0900

    🎨 Change Tab 2 layout from button-based to top button row-based

--- app/src/main/java/com/example/week1/PhotoScreen.kt
+++ app/src/main/java/com/example/week1/PhotoScreen.kt
@@ -10,18 +10,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -29,7 +32,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
@@ -44,15 +46,7 @@ data class Photo(
     @StringRes val addressId: Int,
 )
 
-val photos0 = listOf(
-    Photo(R.drawable.reborn, R.string.photo_name_1, R.string.address_1),
-    Photo(R.drawable.sejong, R.string.photo_name_2, R.string.address_1),
-    Photo(R.drawable.areum, R.string.photo_name_3, R.string.address_1),
-    Photo(R.drawable.plana, R.string.photo_name_4, R.string.address_1),
-    Photo(R.drawable.thebody, R.string.photo_name_5, R.string.address_1),
-    Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
-    Photo(R.drawable.sportscomplex, R.string.photo_name_7, R.string.address_1)
-)
+val sports = listOf("모두", "근력 운동", "클라이밍", "달리기", "수영", "필라테스")
 
 val photos1 = listOf(
     Photo(R.drawable.reborn, R.string.photo_name_1, R.string.address_1),
@@ -64,55 +58,71 @@ val photos2 = listOf(
     Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
 )
 
-val photos = listOf(
-    photos0,
-    photos1,
-    photos2
-
+val photos3 = listOf(
+    Photo(R.drawable.thebody, R.string.photo_name_5, R.string.address_1),
+    Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
 )
+
+val photos4 = listOf(
+    Photo(R.drawable.thebody, R.string.photo_name_5, R.string.address_1),
+    Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
+)
+
+val photos5 = listOf(
+    Photo(R.drawable.thebody, R.string.photo_name_5, R.string.address_1),
+    Photo(R.drawable.auditorium, R.string.photo_name_6, R.string.address_1),
+)
+
+val photosList = listOf(
+    photos1,
+    photos2,
+    photos3,
+    photos4,
+    photos5
+)
+
+val photos0 = photosList.flatten().distinct()
+
+val photos = listOf(photos0) + photosList
+
 @Composable
 fun WorkoutScreen(
     modifier: Modifier = Modifier
 ) {
-    var selectedSports by rememberSaveable { mutableStateOf<Int?>(null) }
-    val sports = listOf("Weight Training", "Running", "Climbing")
+    var selectedSports by rememberSaveable { mutableIntStateOf(0) }
 
-    if (selectedSports == null) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-                .background(MaterialTheme.colorScheme.background)
-        ){
-            Text(
-                text = "운동",
-                color = Color.White
-            )
-            LazyColumn {
-                itemsIndexed(sports) {i, sport ->
-                    SportsItem(
-                        sports = sport,
-                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
-                    ) {
-                        selectedSports = i
-                    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        LazyRow() {
+            itemsIndexed(sports) {i, sport ->
+                SportsItem(
+                    sports = sport
+                ) {
+                    selectedSports = i
                 }
             }
         }
-    }
-    else {
-        PhotoScreen (selectedSports!!, modifier) {
-            selectedSports = null
-        }
+        PhotoScreen (selectedSports, modifier.padding(top = 10.dp))
     }
 }
 
 @Composable
 fun SportsItem(
     sports: String,
-    modifier: Modifier = Modifier,
     onClick: () -> Unit
 ) {
-    Button(onClick = onClick) {
+    Button(
+        modifier = Modifier.height(40.dp).padding(3.dp),
+        colors = ButtonColors(
+            MaterialTheme.colorScheme.secondary,
+            Color.Black,
+            MaterialTheme.colorScheme.secondary,
+            MaterialTheme.colorScheme.secondary
+            ),
+        onClick = onClick) {
         Text(sports)
     }
 
@@ -121,13 +131,9 @@ fun SportsItem(
 @Composable
 fun PhotoScreen(
     sports: Int,
-    modifier: Modifier = Modifier,
-    onHomeButton: () -> Unit
+    modifier: Modifier = Modifier
 ) {
     Column {
-        Button(onClick = onHomeButton) {
-            Text(text = "Home")
-        }
         val scrollState = rememberLazyGridState()
         var selectedImageId by rememberSaveable { mutableStateOf<Int?>(null) }
         if (selectedImageId != null) {
